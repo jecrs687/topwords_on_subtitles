2276fa1462b1e15b36351c6364efb8b3a57197be
jecrs687 <emanuelcascone@gmail.com>
2020-02-04 11:56:07 +0100

    updates

--- main.py
+++ main.py
@@ -1,65 +1,52 @@
 import pysrt
 import os, fnmatch
-
 listOfFiles = os.listdir('.')
 pattern = "*.srt"
 conteudo = ''
 numMinimoRepeticoes=0
+porcentOfBoost = 10
+
+def booster(conteudo):
+    """order the firsts items in the array, this is a boost for the order the full vector
+    \n [return array]"""
+    boost = contar_conteudo_e_remover_excesso(conteudo[0:int(len(conteudo)/porcentOfBoost)])
+    boost = ordenar_conteudo(boost)
+    for x in boost:
+        conteudo.remove(x[0])
+        conteudo.insert(boost.index(x), x[0])
+    return conteudo
+
 def contar_conteudo_e_remover_excesso(conteudo):
     """This function get the words and convert her in one array [[word, weight], ....[word,weight]]
     \n [return: array]
     """
-    palavras = []
-    e=0
-    boost =conteudo[0:int(len(conteudo)/50)]
-    x=0
-    while(x<len(boost)):
-        y=x+1
-        d=0
-        while(y<len(boost)):
-            if(boost[x]==boost[y]):
-                del(boost[y])
-                d=d+1
-            y=y+1
-        boost[x]=[boost[x],d]
-        x=x+1
-    boost.sort(key=lambda x: x[1], reverse=True)
-    for x in boost:
-        conteudo[boost.index(x)]=x[0]
-    for x in range(0,len(conteudo)):
-        d = 0
-        for y in range(x+1, len(conteudo)):
-            if(conteudo[x]==conteudo[y-d]):
-                del(conteudo[y-d])
-                d+=1
-        if(len(conteudo)>x):
-            if(d>=numMinimoRepeticoes):
-                palavras.append((conteudo[x], d+1))
-
-
-    return palavras
+    words=[]
+    while(len(conteudo)>0):
+        weight=conteudo.count(conteudo[0])
+        word=conteudo[0]
+        words.append((word, weight))
+        lenght=len(conteudo)
+        x=0
+        while(x<lenght):
+            if(conteudo[x] == word):
+                del(conteudo[x])
+                lenght-=1
+                continue
+            x+=1
+    return words
 
 def filtrar_conteudo(conteudo):
     """this function is the filter for the words, 
     this function get the words and filter her
     \n [return: string, int]"""
     conteudo = conteudo.split()
-    d=0
-    for x in range(0, len(conteudo)):
-        if(conteudo[x-d].isalpha()==0):
-            del(conteudo[x-d])
-            d=d+1
-            continue
-        if(conteudo[x-d].isalnum()==0):
-            del(conteudo[x-d])
-            d=d+1
-            continue
-        if (conteudo[x-d].count('wiki') > 0):
-            del (conteudo[x-d])
-            d = d + 1
-            continue
-    tamanho = len(conteudo)
-    return conteudo,tamanho
+    palavras = []
+    for x in conteudo:
+        if(x.isalpha()):
+            palavras.append(x)
+    tamanho = len(palavras)
+    del(conteudo)
+    return palavras,tamanho
 
 def ordenar_conteudo(conteudo):
     """this function only realize one sort of the words basead in the weight
@@ -92,6 +79,7 @@ print('_____________________________________________________\n\n________________
 print("filtrando conteudo")
 conteudo,tamanho = filtrar_conteudo(conteudo)
 print("contando conteudo e removendo excesso")
+conteudo = booster(conteudo)
 conteudo = contar_conteudo_e_remover_excesso(conteudo)
 print("ordenando conteudo")
 conteudo = ordenar_conteudo(conteudo)
